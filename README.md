2c4e7b16bcef9cccd5af6877ce03ae608deb32af
Josef Doornink <JDoorni@am.trimblecorp.net>
2021-06-29 20:54:07 -0700

    initial project scaffolding

---
+++ README.md
@@ -0,0 +1,6 @@
+# CICD-Buildout - Introduction
+.Net Core application demonstrating CI/CD build out of a simple .NET 5.0 MVC project for both AzDO and GitHub Actions using Helm charts and Azure Kubernetes Service.
+
+## NOTE:
+DEMONSTRATION PURPOSES ONLY - Cloud resources do not exist 
+Demonstrates how deploy to multiple environments.
---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICD_Example.Controllers
+{
+    public class HealthController : Controller
+    {
+        /// <summary>
+        /// Endpoint used for kubernetes health check
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Alive() => Ok(string.Format("{0}:{1}", "Local DateTime", DateTime.Now));
+
+    }
+}
